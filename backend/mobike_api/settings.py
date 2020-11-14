add326062ff4fb6a096884db9f483047e375cda4
Luis Correa Bruna <lu.correab@alumnos.duoc.cl>
2020-11-13 22:03:54 -0300

    feat: create and update from form done

--- backend/mobike_api/settings.py
+++ backend/mobike_api/settings.py
@@ -39,16 +39,19 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.api',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'mobike_api.urls'
@@ -119,4 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,34 +1,145 @@
 <template>
-  <div id="app">
-    {{ msg }}
-  </div>
-  <!-- <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div id="app">
+        <form @submit.prevent="submitForm">
+            <div class="form-group row">
+                <input
+                    type="text"
+                    class="form-control col-3 mx-2"
+                    placeholder="QR Code"
+                    v-model="bicycle.qr_code"
+                />
+                <input
+                    type="date"
+                    class="form-control col-3 mx-2"
+                    placeholder="Purchase date"
+                    v-model="bicycle.purchase_date"
+                />
+                <input
+                    type="text"
+                    class="form-control col-3 mx-2"
+                    placeholder="Brand"
+                    v-model="bicycle.brand"
+                />
+                <input
+                    type="text"
+                    class="form-control col-3 mx-2"
+                    placeholder="Model"
+                    v-model="bicycle.model"
+                />
+                <input
+                    type="number"
+                    class="form-control col-3 mx-2"
+                    placeholder="Metters traveled"
+                    v-model="bicycle.m_traveled"
+                />
+                <input
+                    type="checkbox"
+                    class="form-control col-3 mx-2"
+                    placeholder="Is locked?"
+                    v-model="bicycle.is_locked"
+                />
+                <button class="btn btn-success">Submit</button>
+            </div>
+        </form>
+
+        <table class="table">
+            <thead>
+                <th>QR Code</th>
+                <th>Purchase date</th>
+                <th>Brand</th>
+                <th>Model</th>
+                <th>Metters traveled</th>
+                <th>Is locked?</th>
+            </thead>
+            <tbody>
+                <tr
+                    v-for="bicycle in bicycles"
+                    :key="bicycle.id"
+                    @dblclick="$data.bicycle = bicycle"
+                >
+                    <td>{{ bicycle.qr_code }}</td>
+                    <td>{{ bicycle.purchase_date }}</td>
+                    <td>{{ bicycle.brand }}</td>
+                    <td>{{ bicycle.model }}</td>
+                    <td>{{ bicycle.m_traveled }}</td>
+                    <td>{{ bicycle.is_locked }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  // components: {
-  //   HelloWorld
-  // }
-  data(){
-    return {
-      msg: 'hello world'
-    }
-  }
-}
+    name: 'App',
+
+    data() {
+        return {
+            bicycle: {},
+            bicycles: [],
+        };
+    },
+
+    async created() {
+        await this.getBicycles();
+    },
+
+    methods: {
+        submitForm() {
+            if (this.bicycle.id === undefined) {
+                this.createBicycle();
+            } else {
+                this.editBicycle();
+            }
+        },
+
+        async getBicycles() {
+            var response = await fetch('http://localhost:8000/api/bicycles/');
+            this.bicycles = await response.json();
+        },
+
+        async createBicycle() {
+            await this.getBicycles();
+
+            await fetch('http://localhost:8000/api/bicycles/', {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(this.bicycle),
+            });
+
+            await this.getBicycles();
+        },
+
+        async editBicycle() {
+            await this.getBicycles();
+
+            await fetch(
+                `http://localhost:8000/api/bicycles/${this.bicycle.id}/`,
+                {
+                    method: 'put',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(this.bicycle),
+                }
+            );
+
+            await this.getBicycles();
+            this.bicycle = {};
+        },
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
